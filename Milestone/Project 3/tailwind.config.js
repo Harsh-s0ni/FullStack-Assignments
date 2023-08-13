db3d36c6d9787c976371f7d5c59df614a2da694d
Harsh-s0ni <hsoni6954@gmail.com>
2023-08-13 17:27:32 +0530

    project 3 done

---
+++ Milestone/Project 3/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./distribution/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
